009691854064e9cd96ec1d566eee85879974c8aa
donokao <s1300195@u-aizu.ac.jp>
2023-06-12 16:20:07 +0900

    Greetings added

--- dice.java
+++ dice.java
@@ -1,8 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
+
 class dice{
     public static void main(String[] args){
 	Random rand = new Random();
 	int sum = 0;
+	
+	System.out.println("What is your name?");
+	Scanner sc = new Scanner(System.in);
+	String name = sc.nextLine();
+	System.out.println("Hello, "+name+"!");
+	
 	System.out.println("Rolling dice...");
 	for(int i = 1; i <= 2; i++){
 	    int ran = rand.nextInt(6) + 1;
